784b12738660effc77b14302693ccf9a0d853770
Atharv Mishra <121117806+AtharvMixraw@users.noreply.github.com>
2023-02-26 23:31:52 +0530

    Add files via upload

---
+++ final.js
@@ -0,0 +1,16 @@
+document.querySelector(".btn").addEventListener("click",b1);
+function b1(){
+    window.open("videolink.html");
+}
+document.querySelector(".btn1").addEventListener("click",b2);
+function b2(){
+    window.open("https://atharviiitk.blogspot.com/");
+}
+document.querySelector(".btn2").addEventListener("click",b3);
+function b3(){
+    window.open("img.html");
+}
+document.querySelector(".btn2").addEventListener("click",b4);
+function b4(){
+    window.open("songs.html");
+}
